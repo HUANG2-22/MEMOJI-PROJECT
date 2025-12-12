a75fcb8b0f1c6911d4e51eb4105b284ea286bddd
HUANG2-22 <huangjinpei94@gmail.com>
2025-12-12 20:08:49 +0800

    Adjust emoji size and background color settings

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let uploadedImg = null;    // 用于存储用户上传的图片对象
 let processedCanvas;       // 存储最终的马赛克结果 (PGraphic)
 const targetSize = 900;    // 目标处理尺寸 (900x900)
 const grid = 10;           // 网格间距
-const maxDiameter = grid + 122; // 最大的 emoji 尺寸 (12)
-const minDiameter = 62;        // 最小的 emoji 尺寸 (2)
+const maxDiameter = grid + 2; // 最大的 emoji 尺寸 (12)
+const minDiameter = 2;        // 最小的 emoji 尺寸 (2)
 
 // ---------------------------
 // 1. 预加载图像资源
@@ -93,7 +93,7 @@ function processImage() {
     
     // C. 最终画布：用于绘制马赛克表情符号
     let finalCanvas = createGraphics(targetSize, targetSize);
-    finalCanvas.background(255); // 设置最终马赛克图片的黑色背景
+    finalCanvas.background(0); // 设置最终马赛克图片的黑色背景
     
     // --- 策略二：密度变化参数 ---
     const skipThreshold = 0.5; // 亮度高于 50% (约 128) 才可能跳过
@@ -108,11 +108,11 @@ function processImage() {
                 let pix = tempCanvas.pixels[index]; // 读取原图的像素值 (0-255)
                 
                 // --- 策略二：密度变化 (抖动) ---
-                let brightnessMap = map(pix, 0, 255, 1, 30.0); 
+                let brightnessMap = map(pix, 0, 255, 0.0, 1.0); 
                 
                 if (brightnessMap > skipThreshold) {
                      // 亮度越高，跳过概率越大 (从 0% 跳到 80%)
-                     let skipProbability = map(brightnessMap, skipThreshold, 1.0, 0.1, 0.9); 
+                     let skipProbability = map(brightnessMap, skipThreshold, 1.0, 0.0, 0.8); 
                      
                      if (random(1) < skipProbability) {
                          continue; // 跳过本次绘制
@@ -122,7 +122,7 @@ function processImage() {
                 // --- 策略一：尺寸缩放 (半色调) ---
                 // 亮度越低 (暗区)，直径越大
                 let reversedPix = 255 - pix;
-                let currentDiameter = map(reversedPix, 0, 255, maxDiameter, minDiameter);
+                let currentDiameter = map(reversedPix, 0, 255, minDiameter, maxDiameter);
                 
                 // --- 选择 Emoji 类型 ---
                 let emoji;
@@ -137,7 +137,7 @@ function processImage() {
                 }
                 
                 // 使用动态直径绘制
-                finalCanvas.image(emoji, x, y, currentDiameter*2, currentDiameter*2);
+                finalCanvas.image(emoji, x, y, currentDiameter, currentDiameter);
             }
         }
     }
